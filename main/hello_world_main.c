9ee665f89c6b3fd3c162a57826dfe48ab050b257
Hemant Kapoor <kapoor.hemant@gmail.com>
2022-10-09 11:11:00 +0100

    Added button logic

--- main/hello_world_main.c
+++ main/hello_world_main.c
@@ -20,7 +20,15 @@
 #define GPIO_LED_PIN    GPIO_NUM_22
 #define GPIO_LED_PIN_SEL (1ULL << GPIO_LED_PIN)
 
-static void configurelEDPin(void);
+#define GPIO_BUTTON_PIN    GPIO_NUM_21
+#define GPIO_BUTTOM_PIN_SEL (1ULL << GPIO_BUTTON_PIN)
+#define DEBOUNCE_TIME_MS (1000U)
+
+static void configureGpio(void);
+static void handleButtonPress(void* arg);
+
+static volatile uint32_t currentButtonLevel = 1;
+static volatile uint32_t currentTimeMs = 0;
 
 void app_main(void)
 {
@@ -42,7 +50,7 @@ void app_main(void)
 
     printf("Minimum free heap size: %d bytes\n", esp_get_minimum_free_heap_size());
 
-    configurelEDPin();
+    configureGpio();
     initThermistor();
 
     pcd8544_config_t config = {
@@ -60,29 +68,74 @@ void app_main(void)
     	pcd8544_printf("Temp = %0.2f",temperature);
     	pcd8544_sync_and_gc();
 
-        gpio_set_level(GPIO_LED_PIN, 1);
+       // gpio_set_level(GPIO_LED_PIN, 1);
         vTaskDelay(500 / portTICK_PERIOD_MS);
-        gpio_set_level(GPIO_LED_PIN, 0);
+        //gpio_set_level(GPIO_LED_PIN, 0);
         vTaskDelay(500 / portTICK_PERIOD_MS);
+
+        
     }
 }
 
 
 /*********** Static function definition here ************************/
-static void configurelEDPin(void)
+static void configureGpio(void)
 {
-    //zero-initialize the config structure.
-    gpio_config_t io_conf = {};
-    //disable interrupt
-    io_conf.intr_type = GPIO_INTR_DISABLE;
-    //set as output mode
-    io_conf.mode = GPIO_MODE_OUTPUT;
-    //bit mask of the pins that you want to set,e.g.GPIO18/19
-    io_conf.pin_bit_mask = GPIO_LED_PIN_SEL;
-    //disable pull-down mode
-    io_conf.pull_down_en = 0;
-    //disable pull-up mode
-    io_conf.pull_up_en = 0;
-    //configure GPIO with the given settings
-    gpio_config(&io_conf);
+    /***********  Configure LED ****************/
+    {
+        //zero-initialize the config structure.
+        gpio_config_t io_conf = {};
+        //disable interrupt
+        io_conf.intr_type = GPIO_INTR_DISABLE;
+        //set as output mode
+        io_conf.mode = GPIO_MODE_OUTPUT;
+        //bit mask of the pins that you want to set,e.g.GPIO18/19
+        io_conf.pin_bit_mask = GPIO_LED_PIN_SEL;
+        //disable pull-down mode
+        io_conf.pull_down_en = 0;
+        //disable pull-up mode
+        io_conf.pull_up_en = 0;
+        //configure GPIO with the given settings
+        gpio_config(&io_conf);
+         gpio_set_level(GPIO_LED_PIN, 1);
+    }
+
+
+    /***********  Configure Button ****************/
+    {
+        //zero-initialize the config structure.
+        gpio_config_t io_conf = {};
+        //disable interrupt
+        io_conf.intr_type = GPIO_INTR_NEGEDGE;
+        //set as output mode
+        io_conf.mode = GPIO_MODE_INPUT;
+        //bit mask of the pins that you want to set,e.g.GPIO18/19
+        io_conf.pin_bit_mask = GPIO_BUTTOM_PIN_SEL;
+        //disable pull-down mode
+        io_conf.pull_down_en = 0;
+        //disable pull-up mode
+        io_conf.pull_up_en = true;
+        //configure GPIO with the given settings
+        gpio_config(&io_conf);    
+
+        ESP_ERROR_CHECK(gpio_install_isr_service(ESP_INTR_FLAG_LOWMED));
+        ESP_ERROR_CHECK(gpio_isr_handler_add(GPIO_BUTTON_PIN, handleButtonPress,  (void*)GPIO_BUTTON_PIN));
+
+       // gpio_isr_register(handleButtonPress, NULL,ESP_INTR_FLAG_LOWMED,NULL);
+    }
+}
+
+static void handleButtonPress(void* arg)
+{
+    //uint32_t time = xTaskGetTickCount() / portTICK_PERIOD_MS;
+    //if( (currentTimeMs - time) < DEBOUNCE_TIME_MS) { return; }
+    //currentTimeMs = time;
+    uint32_t gpio_num = (uint32_t) arg;
+
+    if(gpio_num == GPIO_BUTTON_PIN)
+    {
+        gpio_set_level(GPIO_LED_PIN, currentButtonLevel);
+        currentButtonLevel = (currentButtonLevel == 0U? 1U : 0U);
+    }
+    //printf("Button Pressed\n");
 }
